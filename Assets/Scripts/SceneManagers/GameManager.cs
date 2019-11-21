847ae0c526bca9ade3c490418588f1e254dd4918
Yu Tanaka <YRK00337@nifty.com>
2019-11-21 22:22:07 +0900

    フレームワークの組み込み

---
+++ Assets/Scripts/SceneManagers/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GreeningEx2019
+{
+    public class GameManager : SceneManagerBase
+    {
+
+        private void Update()
+        {
+#if DEBUG
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                // エンディングへ
+                SceneChanger.ChangeScene(SceneChanger.SceneType.Ending);
+            }
+#endif
+        }
+    }
+}
