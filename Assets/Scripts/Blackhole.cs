303adaa95f898dfe78c4f238e832e4e96b3d0993
tanakaedu <tanaka@am1.jp>
2019-11-27 16:05:25 +0900

    ブラックホール実装完了

--- Assets/Scripts/Blackhole.cs
+++ Assets/Scripts/Blackhole.cs
@@ -22,21 +22,75 @@ namespace HungraviyEx2019
         Rigidbody2D rb;
         Animator anim;
         float Energy= 100f;
-        Vector3 targetPosition;
+        Camera myCamera = null;
+        /// <summary>
+        /// 前回の発生状況
+        /// </summary>
+        bool lastSpawned;
+        /// <summary>
+        /// SpawnかSpawnedの時true、DestroyかHidedの時falseにするようにアニメから設定するフラグ
+        /// </summary>
+        bool animSpawn;
 
         private void Awake()
         {
             rb = GetComponent<Rigidbody2D>();
             anim = GetComponent<Animator>();
+            animSpawn = false;
         }
 
-        void Update()
+        private void FixedUpdate()
         {
             if (!CanMove) return;
 
+            bool mouseClicked = Input.GetMouseButton(0);
+            IsSpawn = (mouseClicked && (Energy > 0));
+            anim.SetBool("Spawn", IsSpawn);
 
+            // 発生アニメでなければ移動なし
+            if (!animSpawn)
+            {
+                lastSpawned = false;
+                return;
+            }
+
+            // 動かす
+            if (myCamera == null)
+            {
+                myCamera = Camera.main;
+            }
+            Vector3 mpos = Input.mousePosition;
+            mpos.z = 1;
+            Vector3 target = myCamera.ScreenToWorldPoint(mpos);
+
+            if (!lastSpawned)
+            {
+                transform.position = target;
+                rb.velocity = Vector3.zero;
+                lastSpawned = true;
+                return;
+            }
+
+            Vector3 move = (target - transform.position) / Time.fixedDeltaTime;
+            move = Vector3.ClampMagnitude(move, speedMax);
+            rb.velocity = move;
         }
 
+        /// <summary>
+        /// アニメがSpawn状態に設定する。
+        /// </summary>
+        public void AnimSpawn()
+        {
+            animSpawn = true;
+        }
+
+        /// <summary>
+        /// アニメが非表示の状態に設定する。
+        /// </summary>
+        public void AnimHide()
+        {
+            animSpawn = false;
+        }
 
     }
 }
