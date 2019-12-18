7748341cb766c9308375c77d9a55642a3b61d3db
tanakaedu <tanaka@am1.jp>
2019-12-18 16:06:08 +0900

    アイテムを食べる処理

--- Assets/Scripts/Graviy.cs
+++ Assets/Scripts/Graviy.cs
@@ -19,6 +19,10 @@ namespace HungraviyEx2019 {
         float energyFood = 0.5f;
         [Tooltip("はらぺこ切り替えエネルギー"), SerializeField]
         float hungryAnim = 0.33f;
+        [Tooltip("アイテムを吸い込む秒数"), SerializeField]
+        float eatItemSeconds = 0.5f;
+        [Tooltip("アイテムを吸い込める最低秒数"), SerializeField]
+        float eatItemMinSeconds = 0.3f;
 
         public enum AnimType
         {
@@ -36,10 +40,12 @@ namespace HungraviyEx2019 {
         /// </summary>
         public const float EnergyMin = -0.1f;
 
+        public static Transform MouthTransform { get; private set; }
+
         /// <summary>
-        /// アイテムを吸い込む秒数
+        /// 同時に食べられるアイテムの数
         /// </summary>
-        public const float EatSeconds = 0.5f;
+        const int EatingMax = 8;
 
         /// <summary>
         /// 無敵秒数
@@ -53,6 +59,9 @@ namespace HungraviyEx2019 {
         static SpriteRenderer spRenderer = null;
         static Suiyose suiyose = null;
         static ContactFilter2D contactFilter2D = new ContactFilter2D();
+        static bool isEating = false;
+        static int eatingCount = 0;
+        static Item[] eatingObjects = new Item[EatingMax];
 
         /// <summary>
         /// 移動可能かどうかのフラグ
@@ -81,6 +90,9 @@ namespace HungraviyEx2019 {
             suiyose = GetComponent<Suiyose>();
             Energy = EnergyMax;
             contactFilter2D.layerMask = LayerMask.GetMask("Map");
+            isEating = false;
+            eatingCount = 0;
+            MouthTransform = transform.Find("MouthPosition").transform;
         }
 
         private void Start()
@@ -135,6 +147,40 @@ namespace HungraviyEx2019 {
 
             // はらぺこアニメ切り替えチェック
             anim.SetLayerWeight(1, Energy < hungryAnim ? 1 : 0);
+
+            // 食べ終わりチェック
+            if (isEating && anim.GetFloat("EatSpeed") < 0)
+            {
+                if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime <= 0)
+                {
+                    anim.SetBool("Inhole", false);
+                    isEating = false;
+                }
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.collider.CompareTag("Item"))
+            {
+                if (eatingCount >= EatingMax) {
+#if UNITY_EDITOR
+                    Debug.Log($"これ以上食べられない");
+#endif
+                    return; 
+                }
+
+                eatingObjects[eatingCount] = collision.collider.GetComponent<Item>();
+                eatingObjects[eatingCount].Eat(eatItemSeconds, eatItemMinSeconds);
+                eatingCount++;
+
+                if (!isEating)
+                {
+                    isEating = true;
+                    anim.SetBool("Inhole", true);
+                    anim.SetFloat("EatSpeed", 1);
+                }
+            }
         }
 
         public void AdjustLeftPosition()
@@ -152,6 +198,33 @@ namespace HungraviyEx2019 {
                 var adjust = transform.position;
                 adjust.x = target.x + capsuleCollider2D.bounds.extents.x;
                 transform.position = adjust;
+            }
+        }
+
+        /// <summary>
+        /// 食べ終わったアイテムのインスタンスを報告。
+        /// </summary>
+        /// <param name="item">食べ終わったインスタンス</param>
+        public void EatDone(Item item)
+        {
+            // エネルギーを増やす
+            Energy = Mathf.Clamp01(Energy + energyFood);
+
+            for (int i=0;i<eatingCount;i++)
+            {
+                if (eatingObjects[i] == item)
+                {
+                    eatingObjects[i] = eatingObjects[eatingCount - 1];
+                    break;
+                }
+            }
+            eatingCount--;
+            Debug.Log($"  eatingCount={eatingCount}");
+
+            // 全て食べていたら、口を閉じる
+            if (eatingCount <= 0)
+            {
+                anim.SetFloat("EatSpeed", -1);
             }
         }
     }
