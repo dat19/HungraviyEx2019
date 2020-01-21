95de94bc1de17675bb12bf90f2ffa45af5d7bc5e
Yu Tanaka <YRK00337@nifty.com>
2020-01-21 16:18:27 +0900

    OpenGLES2対応 / カメラ切り替え

---
+++ Assets/Scripts/SubCameraManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HungraviyEx2019
+{
+    public class SubCameraManager : MonoBehaviour
+    {
+        public static SubCameraManager instance = null;
+
+        static Camera myCamera = null;
+
+        private void Awake()
+        {
+            instance = this;
+            myCamera = GetComponent<Camera>();
+        }
+
+        public static void SetEnable(bool flag)
+        {
+            myCamera.enabled = flag;
+        }
+    }
+}
